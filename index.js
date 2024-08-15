01eef4a3698e84c42a9d8273f5b9aef669f9900a
Collette <garlandcollette@gmail.com>
2024-08-15 11:48:08 -0230

    working on connecting to frontend for login and register

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const cors = require('cors');
 const connectDB = require("./db/db");
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 const port = process.env.PORT || 3000;
 const authRoutes = require("./routes/authRoutes");
 
-app.use(cors());
-app.use(express.json());
+
 app.use(express.urlencoded({ extended: false }));
-app.use("/api", authRoutes);
+app.use("/", authRoutes);
 
 // Connect to the database
 connectDB().then(pool => {
