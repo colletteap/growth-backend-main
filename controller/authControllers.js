54c52677b181f3ab623fd571c649f7fffc3fb1f9
Collette <garlandcollette@gmail.com>
2024-08-24 13:51:59 -0230

    inserted jwt but saying there isn't one - working on this

--- controller/authControllers.js
+++ controller/authControllers.js
@@ -9,7 +9,7 @@ const {
 } = require("../utils/sqlFunctions");
 
 const generateAccessToken = (userId) => {
-  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "15m" });
 };
 
 const generateRefreshToken = (userId) => {
