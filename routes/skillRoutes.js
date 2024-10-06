9e8f72826b7beaa7a3a309af1efb678adad8610e
Collette <garlandcollette@gmail.com>
2024-10-06 16:43:59 -0230

    added authorization to addskillpost

--- routes/skillRoutes.js
+++ routes/skillRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { getSkills, skillSearch, skillInfo, addSkillPost } = require('../controller/skillController');
+const { requiresAuth } = require("../middlewares/authMiddleware");
 
 router.get('/skills', getSkills);
 router.get('/skillSearch', skillSearch);
 router.get('/skillInfo', skillInfo);
-router.post('/addSkillPost', addSkillPost);
+router.post('/addSkillPost', requiresAuth, addSkillPost);
 
 module.exports = router;
