a27f8930836a62810bb95c790b0ea7907e0d783b
Collette <garlandcollette@gmail.com>
2024-10-06 15:40:15 -0230

    updated askadvicecarddata.. all code to match sqlfunctions

--- controller/adviceLandingController.js
+++ controller/adviceLandingController.js
@@ -15,24 +15,14 @@ const getAdviceLanding = async (req, res) => {
   }
 };
 const getAskAdviceCardData = async (req, res) => {
-  try {
-    const pool = await connectDB();
-    if (pool) {
-      pool.query('SELECT * FROM askAdviceCardData', (err, results) => {
-        if (err) {
-          console.error("Error fetching advice data:", err.message);
-          return res.status(500).json({ error: 'Database query error' });
-        }
-        if (results.length === 0) {
-          return res.status(404).json({ error: 'No advice data found' });
-        }
-        res.status(200).json(results);
-      });
-    } else {
-      res.status(500).json({ error: 'No database connection' });
+   try {
+    const records = await getAllRecords('askAdviceCardData'); 
+    if (records.length === 0) {
+      return res.status(404).json({ error: 'No data found' });
     }
+    res.status(200).json(records); 
   } catch (error) {
-    console.error("Database connection error:", error);
+    console.error('Error fetching all data:', error.message);
     res.status(500).json({ error: 'Internal server error' });
   }
 };
