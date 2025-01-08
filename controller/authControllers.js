e0e0240fde45e4455ead4db008b047b34577ba5f
Collette <garlandcollette@gmail.com>
2025-01-08 14:23:23 -0330

    changing jwt payloads

--- controller/authControllers.js
+++ controller/authControllers.js
@@ -9,11 +9,11 @@ const {
 } = require("../utils/sqlFunctions");
 
 const generateAccessToken = (userId) => {
-  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "1d" });
 };
 
 const generateRefreshToken = (userId) => {
-  return jwt.sign({ userId }, process.env.JWT_REFRESH_SECRET, { expiresIn: "7d" });
+  return jwt.sign({ userId }, process.env.JWT_REFRESH_SECRET, { expiresIn: "1d" });
 };
 
 
