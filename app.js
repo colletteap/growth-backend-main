a5a00aa918bd0c8c566d26194c6f580c9ef33bb8
Collette <garlandcollette@gmail.com>
2025-01-08 12:59:44 -0330

    trying to get clean health check

--- app.js
+++ app.js
@@ -8,29 +8,8 @@ const path = require('path');
 
 const app = express();
 
-// const corsOptions = {
-//     origin: (origin, callback) => {
-//       if (process.env.NODE_ENV === 'development') {
-//         callback(null, true); 
-//       } else if (process.env.NODE_ENV === 'production') {
-//         const allowedOrigins = [
-//           'https://colletteap.github.io',
-//           'http://growth.ca-central-1.elasticbeanstalk.com',
-//         ];
-//         if (allowedOrigins.includes(origin)) {
-//           callback(null, true);
-//         } else {
-//           callback(new Error('Not allowed by CORS'));
-//         }
-//       }
-//     },
-//     credentials: true, 
-//   };
-
-// app.use(cors(corsOptions));     THIS CAUSES INTERNAL SERVER ERROR ON EB
-
 app.use(cors({
-  origin: ["https://colletteap.github.io, https://growthnl.com"]
+  origin: ["https://colletteap.github.io", "https://growthnl.com"]
 }));
 app.use(express.json());
 
@@ -39,7 +18,6 @@ const port = process.env.PORT || 80;
 app.get('/health', (req, res) => {
     res.status(200).send('OK'); // Simple response for health checks on target groups load balancer
 });
-
 
 app.get('/', (req, res) => {
     res.redirect('https://colletteap.github.io/growth/');
