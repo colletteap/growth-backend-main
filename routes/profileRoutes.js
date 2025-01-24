eab36d88b1a5dc18448631d8ec942659b120672a
Collette <garlandcollette@gmail.com>
2025-01-24 10:55:33 -0330

    changing filepaths

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const multer = require('../utils/multer');
 
 const { updateProfile, getUserData } = require("../controller/profileControllers");
 const { requiresAuth } = require("../middlewares/authMiddleware");
-const upload = require('../utils/multer');
+const upload = require("../utils/multer");
 
 router.get("/profile", requiresAuth, getUserData); 
 router.put("/profile", requiresAuth, upload.single('profilePicture'), updateProfile); 
